8f7f83b09ae87d0ef9e72a54890d7b35a6445fa0
kalanaranathunga <kalanaranathunga5@gmail.com>
2025-04-28 11:43:36 +0530

    ....

--- client.lua
+++ client.lua
@@ -3,6 +3,17 @@ local lastHealth = 0
 
 local bones = {
     ["HEAD"] = 31086,
+    ["SKULL"] = 31086,
+    ["SKULL_1"] = 31086,
+    ["SKULL_2"] = 31086,
+    ["SKULL_3"] = 31086,
+    ["SKULL_4"] = 31086,
+    ["SKULL_5"] = 31086,
+    ["SKULL_6"] = 31086,
+    ["SKULL_7"] = 31086,
+    ["SKULL_8"] = 31086,
+    ["SKULL_9"] = 31086,
+    ["SKULL_10"] = 31086,
     ["UPPER_BODY"] = 24816,
     ["LOWER_BODY"] = 11816,
     ["LEFT_ARM"] = 61163,
@@ -13,6 +24,7 @@ local bones = {
 
 local damageMultipliers = {
     [bones.HEAD] = 0.5,
+    [Skull] = 0.5,
     [bones.UPPER_BODY] = 1.0,
     [bones.LOWER_BODY] = 0.8,
     [bones.LEFT_ARM] = 0.6,
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 
 author '6t9_scripts'
 description 'Custom Body Part Damage Script'
-version '2.0.0'
+version '2.1.0'
 
 client_script 'client.lua'
 server_script 'server.lua'
