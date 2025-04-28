a94779e72b2e0b2005b99a38fb2ea7925b7c7c83
kalanaranathunga <kalanaranathunga5@gmail.com>
2025-04-28 11:36:37 +0530

    ....

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+-- fxmanifest.lua
+fx_version 'cerulean'
+game 'gta5'
+
+author '6t9_scripts'
+description 'Custom Body Part Damage Script'
+version '2.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
